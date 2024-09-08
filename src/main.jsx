6829b4447524045e35a20bd92f3781f73c262098
Ravi-narola <ravinarola3334@gmail.com>
2024-09-08 21:00:43 +0530

    vercel Route Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
       {
         path: "/blogs/:id",
         element: <SingleBlog/>,
-        loader:  ({ params }) => fetch(`https://blog-json-server-9t91.onrender.com/blogs/${params.id}`)
+        loader:  ({ params }) => fetch(`https://blog-json-server-utu3.onrender.com/blogs/${params.id}`)
       }
     ]
   },
